af2a11fb0364150828c62003a21ea744850aad37
Jules <60539590+JulesG10@users.noreply.github.com>
2021-02-26 22:17:23 +0100

    Upload

--- SimpleGui/build.bat
+++ SimpleGui/build.bat
@@ -1,5 +1,4 @@
-
+@echo off
 cd C:\Win-builds\mingw64\bin
-@REM -g  -std=c++0x -pthread
-g++  "C:\APPS\run\Mini-Apps\SimpleGui\main.cpp" "C:\APPS\run\Mini-Apps\SimpleGui\src\*.cpp" -o "C:\APPS\run\Mini-Apps\SimpleGui\bin\main" -Wall  -I "C:\APPS\run\Mini-Apps\SimpleGui\include" -L "C:\APPS\run\Mini-Apps\SimpleGui\lib" -lmingw32 -lSDL2main -lSDL2_ttf -lSDL2 && cd "C:\APPS\run\Mini-Apps\SimpleGui"
-cd C:\APPS\run\Mini-Apps\SimpleGui
+g++ -g -std=c++0x -pthread "C:\Users\jules\OneDrive\Bureau\SDL\main.cpp" "C:\Users\jules\OneDrive\Bureau\SDL\src\*.cpp" -o "C:\Users\jules\OneDrive\Bureau\SDL\bin\main" -Wall  -I "C:\Users\jules\OneDrive\Bureau\SDL\include" -L "C:\Users\jules\OneDrive\Bureau\SDL\lib" -lmingw32 -lSDL2main -lSDL2_ttf -lSDL2 && cd "C:\Users\jules\OneDrive\Bureau\SDL\"
+cd C:\Users\jules\OneDrive\Bureau\SDL\
